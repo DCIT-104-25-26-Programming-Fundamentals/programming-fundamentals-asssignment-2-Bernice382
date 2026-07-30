383aaf7476090b1c10c66c190e35aa6a531231a6
Bernice382 <ganyobernice27@gmail.com>
2026-07-30 20:25:37 +0000

    Update assignment_08_student_records.cpp

--- assignment_08_student_records.cpp
+++ assignment_08_student_records.cpp
@@ -83,12 +83,6 @@
 #include <iomanip>
 using namespace std;
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <iomanip>
-using namespace std;
-
 struct Student
 {
     string name;
