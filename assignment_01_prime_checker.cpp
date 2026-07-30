33cd9c3eaf30f93d4873dab14c355e2b869e6f6d
Bernice382 <ganyobernice27@gmail.com>
2026-07-30 19:32:50 +0000

    Update assignment_01_prime_checker.cpp

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,44 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+bool isPrime(int number)
+{
+    // Numbers less than 2 are not prime
+    if (number < 2)
+    {
+        return false;
+    }
+
+    // Check if the number has any divisor other than 1 and itself
+    for (int i = 2; i < number; i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number))
+    {
+        cout << number << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
